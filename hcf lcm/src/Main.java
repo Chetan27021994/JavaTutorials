24c4e0d3c7d30bf3b997efe33c82960a7ca598bb
Chetan27021994 <chetanb@ingenioustechnotron.com>
2025-10-22 10:22:03 +0530

    Add files via upload

---
+++ hcf lcm/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        int hcf = findHCF(num1, num2);
+        int lcm = (num1 * num2) / hcf;
+
+
+        System.out.println("HCF of " + num1 + " and " + num2 + " is: " + hcf);
+        System.out.println("LCM of " + num1 + " and " + num2 + " is: " + lcm);
+
+        scanner.close();
+    }
+
+
+    public static int findHCF(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
+
+
+
+
+
